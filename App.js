cee36e0a611067449fdaa2b5a43880dcabc9f7b5
Nick Chaloult <npchaloult@gmail.com>
2020-03-02 19:23:11 -0500

    Introduced StatusBar customizations

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar
+        hidden={ false }
+        translucent={ true }
+        barStyle='dark-content'
+      />
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
