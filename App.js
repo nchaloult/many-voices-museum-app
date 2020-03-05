67ecc5fa8b72008eab595718a736f6e397905b33
Nick Chaloult <npchaloult@gmail.com>
2020-03-04 20:04:21 -0500

    Pulling an image from the S3 bucket that amplify created

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Image, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text } from 'react-native';
 
 import Card from './components/Card';
 import Title from './components/Title';
 
+import Amplify, { Storage } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
 export default function App() {
+  const [imgURL, setImgURL] = useState('https://facebook.github.io/react/logo-og.png');
+
+  useEffect(() => {
+    Storage.get('performance11.jpg')
+      .then((data) => setImgURL(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <>
     <StatusBar
@@ -14,6 +26,10 @@ export default function App() {
       barStyle='dark-content'
     />
     <SafeAreaView style={ styles.container }>
+      <Image
+        source={{ uri: imgURL }}
+        style={{ width: 100, height: 100 }}
+      />
       <Title text='Exhibits' />
       <ScrollView>
         <Card title='One' subtitle='Firstname Lastname' bgColor='blue' />
