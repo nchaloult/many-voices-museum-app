da21c989712e3d57ee4348c4971c99d4157bf9d1
Nick Chaloult <npchaloult@gmail.com>
2020-03-10 05:44:44 -0400

    Now fetching and displaying cards for each folder in S3

--- App.js
+++ App.js
@@ -8,14 +8,120 @@ import Amplify, { Storage } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
 
+// Constructs a list of exhibit objects. Returns a Promise.
+//
+// Each exhibit object, or item in the list returned, looks like this:
+// {
+//   "title": "Exhibit Title",
+//   "author": "Author name",
+//   "description": "More information about the exhibit.",
+//   "images": ["awsImageURL1", "awsImageURL2", etc...]
+// }
+const constructExhibitList = (s3Contents) => {
+  return new Promise((resolve, reject) => {
+    let exhibitList = [];
+    let promises = [];
+
+    // Begin at -1 because when we encouter a new directory, we increment
+    // curExhibitIndex. When we encounter our first directory and increment
+    // this variable, we want curExhibitIndex = 0.
+    let curExhibitIndex = -1;
+
+    // First list item is always for the root, or "public," directory. We can
+    // ignore this, so we start the loop at i = 1.
+    for (let i = 1; i < s3Contents.length; i++) {
+      const curS3Object = s3Contents[i];
+      const curObjFilePath = curS3Object['key'];
+
+      // Check to see if the current item we're on is a directory.
+      if (curS3Object['size'] === 0) {
+        curExhibitIndex++;
+        exhibitList[curExhibitIndex] = {};
+      } else {
+        // Strip out the file extension.
+        const fileExt = curObjFilePath.match(/\.[0-9a-z]+$/i)[0].substr(1);
+
+        if (fileExt.localeCompare('json') === 0) {
+          promises.push(addInfo(curObjFilePath, exhibitList, curExhibitIndex));
+        } else {
+          // This file isn't JSON, so it must be an image.
+          promises.push(addImage(curObjFilePath, exhibitList, curExhibitIndex));
+        }
+      }
+    }
+
+    // Wait for all of the promises to resolve. By then, the output array will
+    // be populated.
+    Promise.all(promises)
+      .then(() => {
+        resolve(exhibitList);
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+// Fetches the JSON file specified by the provided filePath from S3 and adds
+// its contents to the ith object in exhibitList.
+const addInfo = (filePath, exhibitList, i) => {
+  return new Promise((resolve, reject) => {
+    Storage.get(filePath)
+      .then((url) => fetch(url))
+      .then((res) => res.json())
+      .then((data) => {
+        exhibitList[i]['title'] = data['title'];
+        exhibitList[i]['author'] = data['author'];
+        exhibitList[i]['description'] = data['description'];
+
+        resolve();
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+// Fetches the image specified by the provided filePath from S3, generates a
+// URL which points to that image, and adds that URL to the ith object in
+// exhibitList's list of image URLs.
+const addImage = (filePath, exhibitList, i) => {
+  return new Promise((resolve, reject) => {
+    Storage.get(filePath)
+      .then((url) => {
+        if (exhibitList[i]['images'] === undefined) {
+          exhibitList[i]['images'] = [];
+        }
+
+        exhibitList[i]['images'].push(url);
+    
+        resolve();
+      })
+      .catch((err) => reject(err));
+  });
+};
+
 export default function App() {
   const [imgURL, setImgURL] = useState('https://facebook.github.io/react/logo-og.png');
 
+  const [exhibitList, setExhibitList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    Storage.get('performance11.jpg')
-      .then((data) => setImgURL(data))
+    // Traverse the bucket's entire file system.
+    Storage.list('')
+      .then((data) => constructExhibitList(data))
+      .then((exhibitList) => {
+        setExhibitList(exhibitList);
+        setIsLoading(false);
+      })
       .catch((err) => console.error(err));
   }, []);
+
+  let cardContent;
+  if (isLoading) {
+    cardContent = <Card title='Loading' subtitle='Loading' bgColor='gray' />;
+  } else {
+    cardContent = exhibitList.map((exhibit, index) => {
+      return <Card key={ index } title={ exhibit['title'] } subtitle={ exhibit['author'] } bgColor='blue' />;
+    });
+  }
 
   return (
     <>
@@ -32,11 +138,7 @@ export default function App() {
       />
       <Title text='Exhibits' />
       <ScrollView>
-        <Card title='One' subtitle='Firstname Lastname' bgColor='blue' />
-        <Card title='Two' subtitle='Firstname Lastname' bgColor='red' />
-        <Card title='Medium Title' subtitle='Firstname Lastname' bgColor='orange' />
-        <Card title='Even Longer Title' subtitle='Firstname Lastname' bgColor='green' />
-        <Card title='Super Duper Long Title' subtitle='Firstname Lastname' bgColor='yellow' />
+        { cardContent }
       </ScrollView>
     </SafeAreaView>
     </>
