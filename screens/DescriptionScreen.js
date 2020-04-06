31c94063654f3004370cb96285abe8e6e8452e31
Nick Chaloult <npchaloult@gmail.com>
2020-04-06 14:24:37 -0400

    Removed lingering console.log statement

--- screens/DescriptionScreen.js
+++ screens/DescriptionScreen.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function DescriptionScreen(props) {
-    console.log(props);
-
     return (
         <>
             <ScrollView>
